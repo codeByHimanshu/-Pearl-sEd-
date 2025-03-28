86ea4a1eb9f9da3ebdcf323996fdff1f9b30ef98
codeByHimanshu <115619706+codeByHimanshu@users.noreply.github.com>
2025-03-28 17:16:07 +0530

    Navbar componet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,42 @@
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Mentor from "./pages/mentor/Mentor";
+import Dashboard from "./pages/mentor/Dashboard";
+import AddCourse from "./pages/mentor/AddCourse";
+import MyCourses from "./pages/mentor/MyCourses";
+import StudentsEnrolled from "./pages/mentor/StudentsEnrolled";
+import Home from "./pages/student/Home";
+import CourseDetail from "./pages/student/CourseDeatil";
+import CoursesList from "./pages/student/CoursesList";
+import Enrollments from "./pages/student/Enrollments";
+import Player from "./pages/student/player";
+import Navbar from "./components/student/Navbar";
 
 function App() {
- 
-
   return (
-    <>
-     <h1>welcome dude</h1>
-    </>
-  )
+    <BrowserRouter>
+      <div>
+
+        <Navbar />
+
+        <Routes>
+          <Route path="/home" element={<Home />}>
+
+            <Route path="course-detail" element={<CourseDetail />} />
+            <Route path="course-list" element={<CoursesList />} />
+            <Route path="enrollments" element={<Enrollments />} />
+            <Route path="player" element={<Player />} />
+          </Route>
+          <Route path="/mentor" element={<Mentor />}>
+           
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="add-course" element={<AddCourse />} />
+            <Route path="my-course" element={<MyCourses />} />
+            <Route path="students-enrolled" element={<StudentsEnrolled />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
