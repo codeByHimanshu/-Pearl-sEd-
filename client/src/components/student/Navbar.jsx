c6e887ed0a8a3c14c16004883d6889f4f3f8138b
codeByHimanshu <115619706+codeByHimanshu@users.noreply.github.com>
2025-03-29 12:29:18 +0530

    navbar

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -6,12 +6,12 @@ function Navbar() {
   return (
       <>
 <div className={`flex items-center justify-between p-4 ${
-  !isCourseList ? " bg-yellow-500" : "bg-cyan-500"
+  !isCourseList ? " bg-yellow-500" : "bg-cyan-300"
 }`}>
        <img src="logo.png" alt="logo" className="h-12 w-12" />
        <div className="flex items-center space-x-4">
-         <div className="bg-gray-400 px-4 py-2 rounded-md">
-           <button className="mr-4">Become Mentor</button>
+         <div className="px-4 py-2 rounded-md">
+           <button className="mr-4 justify-between px-2 py-2  font-bold">Become Mentor</button>
            <Link to="/my-enrollments" className="text-white">My Enrollments</Link>
         </div>
          <button className="px-4 py-2 bg-blue-500 text-white text-lg rounded-md">
