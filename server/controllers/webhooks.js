0616d039e6feb0033d812edfa25906c19fa5e396
codeByHimanshu <115619706+codeByHimanshu@users.noreply.github.com>
2025-05-17 15:06:52 +0530

    addded some new packages and crteated schema for users

---
+++ server/controllers/webhooks.js
@@ -0,0 +1,2 @@
+import {webhook} from 'svix';
+
---
+++ server/models/UserSchema.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    imageURl: { type: String, required: true },
+    enrolledCourses: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+const User = mongoose.model('User',UserSchema)
+  export default User;
