72a9d3b03a7aaf282e88987e6aa6d972302180b7
codeByHimanshu <115619706+codeByHimanshu@users.noreply.github.com>
2025-05-21 15:35:37 +0530

    wenhook added for clerks data

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,2 +1,21 @@
-import {webhook} from 'svix';
+import {Svix, webhook} from 'svix';
+import User from './models/UserSchema.js'
+import dotenv from 'dotenv'
+dotenv.config()
 
+export const clerkhook = async (req,res) => {
+    try {
+        const whook = new webhook(process.env.CELRK_WEBHOOK_SECRET)
+
+    await whook.verify(JSON.stringify(req.body),{
+        "svix-id":req.headers["svix-id"],
+        "svix-timestamp":req.headers["svix-timestamp"],
+        "svix-signature":req.headers["svix-signature"]
+    })
+
+const {data,type} = req.body
+    } catch (error) {
+        
+    }
+}
+ 
