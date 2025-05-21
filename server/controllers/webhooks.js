2b80ddc69a2b4829bd87b7c969051cd697934532
codeByHimanshu <115619706+codeByHimanshu@users.noreply.github.com>
2025-05-21 17:45:05 +0530

    refactored

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,5 +1,5 @@
 import pkg from "svix";
-const { Svix, Webhook } = pkg
+const { Svix, Webhook } = pkg;
 
 import dotenv from "dotenv";
 import User from "../models/UserSchema.js";
@@ -7,7 +7,7 @@ dotenv.config();
 
 export const clerkhook = async (req, res) => {
   try {
-    const whook = new webhook(process.env.CELRK_WEBHOOK_SECRET);
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
     await whook.verify(JSON.stringify(req.body), {
       "svix-id": req.headers["svix-id"],
@@ -18,34 +18,34 @@ export const clerkhook = async (req, res) => {
     const { data, type } = req.body;
 
     switch (type) {
-      case "user.created":
-        {
-          userInfo = {
-            _id: data.id,
-            email: data.email_addresses[0].email_address,
-            name: data.first_name + " " + data.last_name,
-            imageUrl: data.image_url,
-          };
-          await User.create(userInfo)
-          res.json({})
-          break;
-        }
-
-       case "user.updated" :{
+      case "user.created": {
         const userInfo = {
-                email: data.email_address[0].email_address,
-            name: data.first_name + " " + data.last_name,
-            imageUrl: data.image_url,
-        }
-        await User.findByIdAndUpdate(data.id,userInfo)
-           res.json({})
-          break;
-       }
-       case "user.deleted":{
-        await User.findByIdAndDelete(data.id)
-       }
+          _id: data.id,
+          email: data.email_addresses[0].email_address,
+          name: `${data.first_name} ${data.last_name}`,
+          imageUrl: data.image_url,
+        };
+        await User.create(userInfo);
+        return res.json({});
+      }
+
+      case "user.updated": {
+        const userInfo = {
+          email: data.email_addresses[0].email_address,
+          name: `${data.first_name} ${data.last_name}`,
+          imageUrl: data.image_url,
+        };
+        await User.findByIdAndUpdate(data.id, userInfo);
+        return res.json({});
+      }
+
+      case "user.deleted": {
+        await User.findByIdAndDelete(data.id);
+        return res.json({});
+      }
+
       default:
-         res.status(200).json({ message: "Unhandled event type" });
+        return res.status(200).json({ message: "Unhandled event type" });
     }
   } catch (error) {
     console.error("Webhook error:", error);
